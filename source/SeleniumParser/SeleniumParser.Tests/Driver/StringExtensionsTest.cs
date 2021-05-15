0b857a7fa1b93823daf82060de226965a8b501f8
Glayson Sergio da Silva <glaysons@gmail.com>
2021-05-15 09:49:09 -0400

    Criação das funções utilitárias para gerenciamento dos testes

---
+++ source/SeleniumParser/SeleniumParser.Tests/Driver/StringExtensionsTest.cs
@@ -0,0 +1,48 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SeleniumParser.Driver;
+
+namespace SeleniumParser.Tests.Driver
+{
+	[TestClass]
+	public class StringExtensionsTest
+	{
+
+		[TestMethod]
+		public void SeStringsForemIguaisDeveRetornarVerdadeiro()
+		{
+			"TESTES DE INTEGRAÇÃO".IsEquals("Testes de Integração")
+				.Should()
+				.BeTrue();
+
+			"TESTES DE INTEGRAÇÃO".IsEquals("Testes de Integracao")
+				.Should()
+				.BeFalse();
+		}
+
+		[TestMethod]
+		public void SeConverterTextoParaNumeroDeveGerarResultadoEsperado()
+		{
+			"123".ToInt()
+				.Should()
+				.Be(123);
+
+			"a123".ToInt()
+				.Should()
+				.Be(0);
+		}
+
+		[TestMethod]
+		public void SeUmTextoConterOutroDeveRetornarValorEsperado()
+		{
+			"TESTES DE INTEGRAÇÃO".ContainsText("Integração")
+				.Should()
+				.BeTrue();
+
+			"TESTES DE INTEGRAÇÕES".ContainsText("Integracao")
+				.Should()
+				.BeFalse();
+		}
+
+	}
+}
